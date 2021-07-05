1bbf8eed80178cb7e7e0cac7c68c38fc0cd92376
AzeezFazry <azeezfazry@gmail.com>
2021-07-05 19:03:14 +0530

    Frame is created

---
+++ app/src/main/java/com/example/azeezfazryphoneapplication/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.azeezfazryphoneapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    TextView tvDial;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void buDialAction(View view) {
+    }
+}
