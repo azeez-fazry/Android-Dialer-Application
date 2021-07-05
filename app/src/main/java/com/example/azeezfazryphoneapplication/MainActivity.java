08c2e6ba12d57bfee2a65e63e8cc8924d9821620
AzeezFazry <azeezfazry@gmail.com>
2021-07-05 19:24:28 +0530

    Numbers and delete button is implemented

--- app/src/main/java/com/example/azeezfazryphoneapplication/MainActivity.java
+++ app/src/main/java/com/example/azeezfazryphoneapplication/MainActivity.java
@@ -4,7 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     TextView tvDial;
@@ -12,8 +14,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tvDial = findViewById(R.id.tvPhoneNumber);
     }
 
     public void buDialAction(View view) {
+        Button button = (Button)view;
+        String digit = button.getText().toString();
+        String phoneNumber = tvDial.getText().toString();
+        tvDial.setText(phoneNumber + digit);
+    }
+
+
+    public void buDeleteAction(View view) {
+        try {
+            String phoneNumber = tvDial.getText().toString();
+            StringBuffer stringBuffer = new StringBuffer(phoneNumber);
+            stringBuffer.deleteCharAt(stringBuffer.length() - 1);
+            tvDial.setText(stringBuffer.toString());
+        } catch (Exception e) {
+            Toast.makeText(this,"Phone number is empty", Toast.LENGTH_SHORT).show();
+        }
     }
 }
